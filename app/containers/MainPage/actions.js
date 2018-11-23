b040b520328f45b691fa26a23ed31b499c71375d
Luke Baziw-Grossmann <luke@Lukes-MacBook-Pro.local>
2018-11-22 21:13:32 -0800

    Setup actions for main page

--- app/containers/MainPage/actions.js
+++ app/containers/MainPage/actions.js
@@ -4,10 +4,58 @@
  *
  */
 
-import { DEFAULT_ACTION } from './constants';
+import { 
+	LOAD_EVENTS, 
+	LOAD_EVENTS_SUCCESS, 
+	LOAD_EVENTS_ERROR, 
+	LOAD_FEATURED_EVENTS,
+	LOAD_FEATURED_EVENTS_SUCCESS,
+	LOAD_FEATURED_EVENTS_ERROR 
+	} from './constants';
 
-export function defaultAction() {
+export function loadEvents(tenantId, skip, take, searchTerm) {
   return {
-    type: DEFAULT_ACTION,
+    type: LOAD_EVENTS,
+    tenantId,
+    skip,
+    take,
+    searchTerm
+  };
+}
+
+export function loadEventsError(error) {
+  return {
+    type: LOAD_EVENTS_ERROR,
+   	error
+  };
+}
+
+export function loadFeaturedEvents(tenantId, skip, take) {
+  return {
+    type: LOAD_FEATURED_EVENTS,
+   	tenantId,
+   	skip,
+   	take
+  };
+}
+
+export function loadFeaturedEventsSuccess(featuredEvents) {
+  return {
+    type: LOAD_FEATURED_EVENTS_SUCCESS,
+   	featuredEvents
+  };
+}
+
+export function loadFeaturedEventsError(error) {
+  return {
+    type: LOAD_FEATURED_EVENTS_SUCCESS,
+   	error
+  };
+}
+
+export function loadEventsSuccess(events) {
+  return {
+    type: LOAD_EVENTS_SUCCESS,
+   	events
   };
 }
--- app/containers/MainPage/constants.js
+++ app/containers/MainPage/constants.js
@@ -4,4 +4,15 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/MainPage/DEFAULT_ACTION';
+// Actions for featured events
+export const LOAD_FEATURED_EVENTS = 'app/MainPage/LOAD_FEATURED_EVENTS';
+export const LOAD_FEATURED_EVENTS_SUCCESS = 'app/MainPage/LOAD_FEATURED_EVENTS_SUCCESS';
+export const LOAD_FEATURED_EVENTS_ERROR = 'app/MainPage/LOAD_FEATURED_EVENTS_ERROR';
+
+// Actions for regular events 
+
+export const LOAD_EVENTS = 'app/MainPage/LOAD_EVENTS';
+export const LOAD_EVENTS_SUCCESS = 'app/MainPage/LOAD_EVENTS_SUCCESS';
+export const LOAD_EVENTS_ERROR = 'app/MainPage/LOAD_EVENTS_ERROR';
+
+
